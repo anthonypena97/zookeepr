5c5d11ff1b36ba03b59340b14b9495285ffb507d
Anthony Pena <apena5@alumni.jh.edu>
2021-08-07 13:14:01 -0600

    added id param

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+const { ESRCH } = require('constants');
 const express = require('express')
 const { animals } = require('./data/animals')
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result
+}
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -49,6 +55,15 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result)
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
